e7aff42bbcf9fa6e63844f6f05dae856773aab1d
xtakac09 <xtakac09@stud.fit.vutbr.cz>
2023-03-19 21:51:53 +0100

    interrupt debugovan

--- ipkcp.c
+++ ipkcp.c
@@ -11,15 +11,9 @@
 #define BUFFER_SIZE_UDP 512     
 #define BUFFER_SIZE_TCP 1024     
 
-// TODO
-//  - check adresy v argumentu
-
-
 // rozdil mezi win a unix bude mozna v adresach neceho, struktura sockaddr_in nebo tak
 
-static volatile int keepRunning = 1;
-// int interruptSocket;                                                            // Global integer for socket because of interrupt signal, for communication is still used original variable
-// char mode[4];                                                                   // Same reason as line above       
+static volatile int keepRunning = 1;                                                    // Help variable in case of interrupt
 
 void intHandler() {
     keepRunning = 0;
@@ -27,10 +21,10 @@ void intHandler() {
 }
 
 int main(int argc, char** argv){
-    signal(SIGINT, intHandler);                                     // For C-c interrupt
+    signal(SIGINT, intHandler);                                                         // For C-c interrupt
 
 // Parameter check 
-    if(argc != 7){                                                  // Number of args
+    if(argc != 7){                                                                      // Number of args
         fprintf(stderr, "ERROR: Wrong number of arguments. %d of 6 needed.\n", argc);
         return 1;
     }
@@ -45,48 +39,46 @@ int main(int argc, char** argv){
         return 1;
     }
 
-    if(atoi(argv[4]) < 0){                                          // Checks if port number isn't negative
+    if(atoi(argv[4]) < 0){                                                              // Checks if port number isn't negative
         fprintf(stderr, "ERROR: Wrong port parameter: %s.\n", argv[4]);
         return 1;
     }
 
-    char conType[4];                                                // Connection type (UDP/TCP)
+    char conType[4];                                                                    // Connection type (UDP/TCP)
     strcpy(conType, argv[6]);
 
     char host_address[16];               
     strcpy(host_address, argv[2]);
-
-    //strcpy(mode, conType);
 
     int port_number = atoi(argv[4]);
     char buffer_udp[BUFFER_SIZE_UDP];
     char buffer_tcp[BUFFER_SIZE_TCP];
     char input[BUFFER_SIZE_UDP];
     char input_tcp[BUFFER_SIZE_TCP];
-    int family = AF_INET;                                                       // ipv4
+    int family = AF_INET;                                                               // ipv4
 
     // Getting the message to send, getting server info 
-    struct hostent *server = gethostbyname(host_address);                       // Gets info about server. Parameter takes either address or name
-    if(server == NULL){                                                         // Invalid server name
+    struct hostent *server = gethostbyname(host_address);                               // Gets info about server. Parameter takes either address or name
+    if(server == NULL){                                                                 // Invalid server name
         fprintf(stderr, "ERROR: no such host %s.\n", host_address);
         return 1;
     }
 
     struct sockaddr_in server_address;
-    memset(&server_address, 0, sizeof(server_address));                         // fills server address with zeros
+    memset(&server_address, 0, sizeof(server_address));                                 // fills server address with zeros
 
     server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(port_number);                               // sets port that the socket will use. htonl() translates an unsigned long integer into network byte order
+    server_address.sin_port = htons(port_number);                                       // sets port that the socket will use. htonl() translates an unsigned long integer into network byte order
     memcpy(&server_address.sin_addr.s_addr, server->h_addr_list[0], server->h_length); 
 
     printf("INFO: Server socket: %s : %d \n", 
-            inet_ntoa(server_address.sin_addr),                                  // server's in address
-            ntohs(server_address.sin_port));                                     // server's in port
+            inet_ntoa(server_address.sin_addr),                                         // server's in address
+            ntohs(server_address.sin_port));                                            // server's in port
 
     socklen_t server_size = sizeof(server_address);    
 
 // UDP ------------------------------------------------------------------------------------------
-    if(strcmp(conType, "udp") == 0){        // UDP - no need to disconnect if an error occurs
+    if(strcmp(conType, "udp") == 0){                                                    // UDP - no need to disconnect if an error occurs
         int type = SOCK_DGRAM;
 
     // SOCKET() - creating socket
@@ -96,13 +88,11 @@ int main(int argc, char** argv){
             return 1;
         }
 
-        //interruptSocket = client_socket;
-
         while(keepRunning){
         // MESSAGE
-            memset(buffer_udp, 0, BUFFER_SIZE_UDP);                                 // Fills buffer with 0
+            memset(buffer_udp, 0, BUFFER_SIZE_UDP);                                     // Fills buffer with 0
             
-            if(fgets(input, BUFFER_SIZE_UDP, stdin) == NULL){                       // Reads from input 
+            if(fgets(input, BUFFER_SIZE_UDP, stdin) == NULL){                           // Reads from input 
                 fprintf(stderr, "ERROR occured while reading input.\n");
                 return 1;
             }
@@ -111,16 +101,16 @@ int main(int argc, char** argv){
                 break;
             }
 
-            if(strcmp(input, "exit\n") == 0){                                   // Exit message
+            if(strcmp(input, "exit\n") == 0){                                           // Exit message
                 printf("Exiting...\n");
                 return 0;
             }
 
-            int inputLength = (int) strlen(input) - 1;                          // - 1, because '\n' is not considered as part of the payload length
-            buffer_udp[0] = 0;                                                      // Opcode (0 = request)
-            buffer_udp[1] = inputLength;                                            // Payload Length
+            int inputLength = (int) strlen(input) - 1;                                  // - 1, because '\n' is not considered as part of the payload length
+            buffer_udp[0] = 0;                                                          // Opcode (0 = request)
+            buffer_udp[1] = inputLength;                                                // Payload Length
 
-            for(int i  = 0; i < inputLength; i++){                              // Fills the payload data area with user input
+            for(int i  = 0; i < inputLength; i++){                                      // Fills the payload data area with user input
                 buffer_udp[i + 2] = input[i];                                      
             }
 
@@ -130,7 +120,7 @@ int main(int argc, char** argv){
                 fprintf(stderr, "ERROR: sendto.\n");
             }
 
-            memset(buffer_udp, 0, sizeof(buffer_udp));                                   // Fills buffer with 0
+            memset(buffer_udp, 0, sizeof(buffer_udp));                                  // Fills buffer with 0
             
         // RECVFROM() - waiting for response
             
@@ -147,7 +137,7 @@ int main(int argc, char** argv){
                 for(int i = 3; i < responseLength + 3; i++){
                     printf("%c", buffer_udp[i]);
                 }
-            } else {                            // Had to duplicate it because of the new line (ERR sends \n, OK does not)
+            } else {                                                                    // Had to duplicate it because of the new line (ERR sends \n, OK does not)
                 printf("OK:");
                 for(int i = 3; i < responseLength + 3; i++){
                     printf("%c", buffer_udp[i]);
@@ -210,8 +200,7 @@ int main(int argc, char** argv){
         }
 
     // CLOSE()
-        if(!keepRunning){ 
-            printf("zmacklo se C-c, ukoncuju.\n");
+        if(!keepRunning){                                                               // If interrupted, this sends BYE and waits for response
             memset(buffer_tcp, 0, BUFFER_SIZE_TCP); 
             strcpy(buffer_tcp, input_tcp);
 
@@ -220,7 +209,6 @@ int main(int argc, char** argv){
                 fprintf(stderr, "ERROR: send.\n");
             }
 
-            printf("za sendem\n");
             memset(buffer_tcp, 0, BUFFER_SIZE_TCP); 
             
             int bytes_sx = recv(client_socket, buffer_tcp, BUFFER_SIZE_TCP, 0);
@@ -228,14 +216,10 @@ int main(int argc, char** argv){
                 fprintf(stderr, "ERROR: recv.\n");
             }
 
-            printf("za recv\n");
             printf("%s", buffer_tcp);
-            printf("za printosem odpovedi\n");
 
-        } else {
-            printf("ukoncuju\n");
         }
-        printf("jeste su tu3 u close\n");
+       
         shutdown(client_socket, SHUT_RD);
         shutdown(client_socket, SHUT_WR);
         shutdown(client_socket, SHUT_RDWR);
